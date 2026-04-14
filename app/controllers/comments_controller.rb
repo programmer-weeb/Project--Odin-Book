3abe07758288617e4d604a608081c0f45772e6e9
Ahmed Melouk <forprogramming8@gmail.com>
2026-04-14 14:23:53 +0200

    Implement CommentsController#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: :create
+  before_action :set_comment, only: :destroy
+
   def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to post_path(@post), notice: "Comment created."
+    else
+      redirect_to post_path(@post), alert: @comment.errors.full_messages.to_sentence
+    end
   end
 
   def destroy
   end
+
 end
