68726586009fe25f04a4d031e9a844b6220367ad
Ahmed Melouk <forprogramming8@gmail.com>
2026-04-14 14:23:53 +0200

    Implement LikesController#create

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,19 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: :create
+  before_action :set_like, only: :destroy
+
   def create
+    @like = @post.likes.build(user: current_user)
+
+    if @like.save
+      redirect_to post_path(@post), notice: "Post liked."
+    else
+      redirect_to post_path(@post), alert: @like.errors.full_messages.to_sentence
+    end
   end
 
   def destroy
   end
+
 end
