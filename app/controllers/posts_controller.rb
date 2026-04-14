7132dcb78cd45cd4bccec3c87e59799032daa9eb
Ahmed Melouk <forprogramming8@gmail.com>
2026-04-14 14:23:53 +0200

    Implement PostsController#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_post_owner!, only: [:edit, :update, :destroy]
+
   def index
+    @post = current_user.posts.build
+    @posts = Post.includes(:user, :comments, :likes).order(created_at: :desc)
   end
 
   def show
@@ -19,4 +25,5 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
 end
