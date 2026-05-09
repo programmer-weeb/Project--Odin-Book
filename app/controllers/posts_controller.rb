dd5ccea31bcfe75f7692a8ca75e2a9533adc5711
Ahmed Melouk <forprogramming8@gmail.com>
2026-05-09 23:45:00 +0300

    perf: trim dead eager-loads and fix comment N+1 in posts#show
    Index/create only need user:profile (card uses counter caches + display_name).
    Show now inlines its own preloaded Post.find and includes user:profile on
    comments. Edit/update/destroy get a bare Post.find via set_post. Users#show
    drops the posts includes since post.user resolves to the already-loaded @user.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_post, only: [ :edit, :update, :destroy ]
   before_action :authorize_post_owner!, only: [ :edit, :update, :destroy ]
 
   def index
     @post = current_user.posts.build
-    @posts = Post.includes(:likes, user: :profile, comments: :user).order(created_at: :desc)
+    @posts = Post.includes(user: :profile).order(created_at: :desc)
     @likes_by_post_id = current_user.likes.where(post_id: @posts.map(&:id)).index_by(&:post_id)
   end
 
   def show
+    @post = Post.includes(user: :profile).find(params[:id])
     @comment = @post.comments.build
-    @comments = @post.comments.includes(:user).order(created_at: :asc)
+    @comments = @post.comments.includes(user: :profile).order(created_at: :asc)
     @likes_by_post_id = current_user.likes.where(post_id: @post.id).index_by(&:post_id)
   end
 
@@ -25,7 +26,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to post_path(@post), notice: "Post created."
     else
-      @posts = Post.includes(:likes, user: :profile, comments: :user).order(created_at: :desc)
+      @posts = Post.includes(user: :profile).order(created_at: :desc)
       @likes_by_post_id = current_user.likes.where(post_id: @posts.map(&:id)).index_by(&:post_id)
       render :index, status: :unprocessable_entity
     end
@@ -50,7 +51,7 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    @post = Post.includes(user: :profile).find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def authorize_post_owner!
